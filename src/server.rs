8f79aa23659f5dcfcf40c2c36335ba08f96cf95a
rkrishn7 <rkrishn7@users.noreply.github.com>
2023-11-07 00:21:10 -0800

    more

--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,35 @@
-use rdkafka::message::OwnedMessage;
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 
-use crate::ws::WSConnection;
+use crate::{topic::TopicBroadcastChannel, ws::WSConnection};
 
-pub async fn serve(listen_addr: &SocketAddr) -> anyhow::Result<()> {
+pub async fn serve<T>(
+    listen_addr: &SocketAddr,
+    topic_broadcast_map: Arc<HashMap<String, TopicBroadcastChannel<T>>>,
+) -> anyhow::Result<()>
+where
+    T: Clone + Send + 'static,
+{
     let listener = TcpListener::bind(listen_addr).await?;
     tracing::info!("Server started. Listening on: {}", listen_addr);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(async {
+    while let Ok((stream, addr)) = listener.accept().await {
+        let topic_broadcast_map = Arc::clone(&topic_broadcast_map);
+        tokio::spawn(async move {
             match tokio_tungstenite::accept_async(stream).await {
-                Ok(ws_stream) => {
+                Ok(mut ws_stream) => {
                     tracing::info!("New WebSocket connection: {:?}", ws_stream);
 
-                    let connection = WSConnection::<OwnedMessage>::new(Arc::new(HashMap::new()));
+                    let mut connection = WSConnection::<T>::new(topic_broadcast_map);
 
-                    connection.consume(ws_stream).await;
+                    let _ = connection.consume(&mut ws_stream).await;
                 }
                 Err(err) => {
-                    tracing::error!("Error during the websocket handshake occurred: {}", err);
+                    tracing::error!(
+                        "Error during websocket handshake occurred for client {}: {}",
+                        addr,
+                        err
+                    );
                 }
             }
         });
