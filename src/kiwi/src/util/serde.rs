a07614c0b743951a573ce2990b870958b6593854
rkrishn7 <rkrishn7@users.noreply.github.com>
2024-01-29 23:57:11 -0800

    fix clippy warnings

--- src/kiwi/src/util/serde.rs
+++ src/kiwi/src/util/serde.rs
@@ -4,10 +4,10 @@ pub mod base64 {
     use serde::{Deserializer, Serializer};
 
     pub fn serialize<S: Serializer>(v: &Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error> {
-        let base64 = match v {
-            Some(v) => Some(base64::engine::general_purpose::STANDARD.encode(v)),
-            None => None,
-        };
+        let base64 = v
+            .as_ref()
+            .map(|v| base64::engine::general_purpose::STANDARD.encode(v));
+
         <Option<String>>::serialize(&base64, s)
     }
 
@@ -16,8 +16,8 @@ pub mod base64 {
         match base64 {
             Some(v) => base64::engine::general_purpose::STANDARD
                 .decode(v.as_bytes())
-                .map(|v| Some(v))
-                .map_err(|e| serde::de::Error::custom(e)),
+                .map(Some)
+                .map_err(serde::de::Error::custom),
             None => Ok(None),
         }
     }
