227198e506bebaab858d8b8edbd67b32e70beaad
rkrishn7 <rkrishn7@users.noreply.github.com>
2024-03-08 23:23:25 -0800

    chore: add lifecycle tests

---
+++ src/kiwi/tests/lifecycle.rs
@@ -0,0 +1,62 @@
+mod common;
+
+use std::time::Duration;
+
+use common::kiwi::{ConfigFile, Process};
+use nix::sys::signal;
+
+static GRACE_PERIOD_MS: u64 = 500;
+
+#[tokio::test]
+async fn test_shuts_down_sigint() -> anyhow::Result<()> {
+    let config = ConfigFile::from_str(
+        format!(
+            r#"
+        sources: []
+        server:
+            address: '127.0.0.1:8000'
+        "#
+        )
+        .as_str(),
+    )?;
+
+    let mut kiwi = Process::new_with_args(&["--config", config.path_str()])?;
+
+    kiwi.signal(signal::SIGINT)?;
+
+    tokio::time::sleep(Duration::from_millis(GRACE_PERIOD_MS)).await;
+
+    let status = kiwi.proc_mut().try_wait()?;
+
+    assert!(status.is_some());
+    assert_eq!(status.unwrap().code(), Some(0));
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_shuts_down_sigterm() -> anyhow::Result<()> {
+    let config = ConfigFile::from_str(
+        format!(
+            r#"
+        sources: []
+        server:
+            address: '127.0.0.1:8000'
+        "#
+        )
+        .as_str(),
+    )?;
+
+    let mut kiwi = Process::new_with_args(&["--config", config.path_str()])?;
+
+    kiwi.signal(signal::SIGTERM)?;
+
+    tokio::time::sleep(Duration::from_millis(GRACE_PERIOD_MS)).await;
+
+    let status = kiwi.proc_mut().try_wait()?;
+
+    assert!(status.is_some());
+    assert_eq!(status.unwrap().code(), Some(0));
+
+    Ok(())
+}
